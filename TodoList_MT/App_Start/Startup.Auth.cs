d4cf2de4512901192662e653c1c1245c4d058af0
Kalyan Krishna <kalyan.krishna@microsoft.com>
2019-01-21 00:30:32 -0800

    NEw solution created

---
+++ TodoList_MT/App_Start/Startup.Auth.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Claims;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using TodoList_MT.Models;
+
+namespace TodoList_MT
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private string graphResourceID = "https://graph.windows.net";
+        private static string aadInstance = EnsureTrailingSlash(ConfigurationManager.AppSettings["ida:AADInstance"]);
+        private string authority = aadInstance + "common";
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { });
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // instead of using the default validation (validating against a single issuer value, as we do in line of business apps), 
+                        // we inject our own multitenant validation logic
+                        ValidateIssuer = false,
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        SecurityTokenValidated = (context) => 
+                        {
+                            return Task.FromResult(0);
+                        },
+                        AuthorizationCodeReceived = (context) =>
+                        {
+                            var code = context.Code;
+
+                            ClientCredential credential = new ClientCredential(clientId, appKey);
+                            string tenantID = context.AuthenticationTicket.Identity.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+                            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                            AuthenticationContext authContext = new AuthenticationContext(aadInstance + tenantID, new ADALTokenCache(signedInUserID));
+                            AuthenticationResult result = authContext.AcquireTokenByAuthorizationCodeAsync(
+                                code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, graphResourceID).Result;
+
+                            return Task.FromResult(0);
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                            context.OwinContext.Response.Redirect("/Home/Error");
+                            context.HandleResponse(); // Suppress the exception
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+
+        }
+
+        private static string EnsureTrailingSlash(string value)
+        {
+            if (value == null)
+            {
+                value = string.Empty;
+            }
+
+            if (!value.EndsWith("/", StringComparison.Ordinal))
+            {
+                return value + "/";
+            }
+
+            return value;
+        }
+    }
+}
---
+++ TodoList_MT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodoList_MT.MvcApplication" Language="C#" %>
